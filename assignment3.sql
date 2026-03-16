60e090de5e89316bc643f7098337b832d1f1562f
Sanika3796 <sanikapawar2903@gmail.com>
2026-03-16 10:40:52 +0530

    Create assignment3.sql

---
+++ assignment3.sql
@@ -0,0 +1,8 @@
+Assignment 3
+Overview of SQL
+
+1) Does ANSI recognize the data type DATE?
+Answer: Yes, ANSI SQL recognizes DATE as a standard data type used to store date values.
+
+2) Which subdivision of SQL is used to insert values in tables?
+Answer: DML (Data Manipulation Language) is used to insert values into tables. The INSERT statement is part of DML.
