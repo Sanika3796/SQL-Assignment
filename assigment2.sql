1d5ac775b76db94e6742a789d844457994e921a6
Sanika3796 <sanikapawar2903@gmail.com>
2026-03-16 10:29:13 +0530

    Create assigment2.sql

---
+++ assigment2.sql
@@ -0,0 +1,16 @@
+Assignment 2
+Introducing Relational Database
+
+1) Which field of the Customers table is the primary key?
+Answer:Cnum is the primary key of the Customers table because it uniquely identifies each customer.
+
+2) What is the 4th column of the Customers table?
+Answer:The 4th column is Rating.
+
+3) What is another word for row? For column?
+Answer:Another word for row is record or tuple.
+Another word for column is field or attribute.
+
+4) Why isn’t it possible to see the first five rows of a table?
+Answer:Because tables in a relational database do not store rows in a fixed order.
+Rows are returned only when a SELECT query with ORDER BY is used.
